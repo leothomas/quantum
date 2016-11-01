50aec4a18f07532e57dc9b9b4716b83056937a25
Leo Thomas <leo@identify3d.com>
2016-10-31 22:16:39 -0700

    added comparison to entangled + traced qubits

--- fourier.py
+++ fourier.py
@@ -5,34 +5,21 @@ import numpy as np
 import scipy as scp
 import matplotlib.pyplot as plt
 
+def inverse_2x2(m):
+	if (m[0][0]*m[1][1] != m[0][1]*m[1][0]):
+		return 1/(m[0][0]*m[1][1]- m[0][1]*m[1][0]) * \
+					np.array([[m[1][1], -m[0][1]], [-m[1][0], m[0][0]]] ) 
+	else:
+		print "Error: Singular matrix. E: ", E
 # matrix can be any hamiltonian (in matrix form, NOT qubit object). This can be 
 # changed if necessary
 def GE(matrix, E):
-		threshold = 0.00001
 		n = len(matrix)
-		Greens = (E*qeye(n) - matrix).full()
-		# cond(Greens) increases as the matrix approaches singularity. 
-		# These conditionals catch near singular matrix and subtract 
-		# i*Epsilon such that the determinant is never = 0
-		# THIS IS THE MOST COMPUTATIONALLY INTENSIVE ASPECT OF THIS PROGRAM
-		if np.linalg.cond(Greens) < 1/threshold:
-			return np.linalg.inv(Greens)
-		else:
-			print "Bad condition matrix at E = ", E
-			#return np.array([[0,0],[0,0]])
-			return np.linalg.inv((1j*0.0001*qeye(n) - matrix).full())
-
-# tail recursive implementation of a fourrier transform.
-# Python limits number of recursions, so this algorithm is not 
-# particularly helpful. I have left if for reference
-step_rec = 0.001
-def fourier_transform_rec(func, t):
-	def ft(E,crt):
-		if E <= 4:
-			return ft(E+step_rec, step_rec*(crt+(func(E)*np.exp(-1j*t*E))))
-		else:
-			return crt
-	return ft 
+		Greens = ( (E-10**(-10)*1j)*qeye(n) - matrix).full()
+		#Greens = (E*qeye(n) - matrix).full()
+		#if np.linalg.cond(Greens) < 1/1000:
+		#		Greens = ( (E-10**(-10)*1j)*qeye(n) - matrix).full()
+		return np.linalg.pinv(Greens)
 
 # iterative implementation of fourrier transform using left hand rectangle
 # rule for integration
@@ -52,19 +39,3 @@ def fourier_transform_loop(func, Emax, dx, t):
 		E = E+dx
 		# multiplies total sum by dx
 	return dx*crt
-
-# implementation using built in integration function to integrate more efficiently
-# and using greater limits. Issues with return type of func(x)
-def fourier_transform(func, t):
-	return scp.integrate.quad(lambda x: func(x)*np.exp(-1j*t*x), -scp.inf, scp.inf)
-
-# implementation using trapz, numerical integration using trapezoids. Run-time
-# issues present. Havent had time to debug. 
-def fourier_trans(func, t):
-	dx = 0.001
-	x = np.arange(-15, 15, dx)
-	fun = np.zeros(len(x))
-	for i in range(0, len(x)):
-		fun[i] = func(x[i])
-		i=i+1
-	return scp.trapz(fun * np.exp(-1j*t*x))
--- test.py
+++ test.py
@@ -14,8 +14,8 @@ import fourier as fourier
 import qubit as qubit
 
 # initializing 2 qubit objects (Eup, Edown, coupling)
-qubitR = qubit.qubit(2.0, 2.0, 0.5)
-qubitL = qubit.qubit(0.5, 1.0, 1.0)
+qubitR = qubit.qubit(-2.0, 1.0,  0.75)
+qubitL = qubit.qubit(0.5, -0.1, 0.2)
 
 qubitEnt = tensor(qubitR.twolevel(), qubitL.twolevel())
 print qubitR.twolevel()
@@ -23,63 +23,127 @@ print qubitL.twolevel()
 print qubitEnt
 print "ptrace(0): ", qubitEnt.ptrace(0)
 print "ptrace(1): ", qubitEnt.ptrace(1)
+print "tracedist (0, L) : ", tracedist(qubitEnt.ptrace(0), qubitL.twolevel())
+print "tracedist (1, R) : ", tracedist(qubitEnt.ptrace(1), qubitR.twolevel())
+print "tracedist (1, L) : ", tracedist(qubitEnt.ptrace(1), qubitL.twolevel())
+print "tracedist (0, R) : ", tracedist(qubitEnt.ptrace(0), qubitR.twolevel())
+
+channel_length = 5
+rand_ch = qubit.channel(channel_length)
+energies = np.random.uniform(low=(-1.0), high=1.0, size =(channel_length))
+rand_ch.setEnergies(energies)
+system_rand = tensor(qubitL.twolevel(), rand_ch.asQobj(), qubitR.twolevel())
+ptrace_rand = system_rand.ptrace(2)
+result_rand = qubit.qubit(ptrace_rand.full()[0][0],\
+	ptrace_rand.full()[1][1],ptrace_rand.full()[0][1])
+
+id_ch = qubit.channel(channel_length)
+e = 1
+energies0 = np.zeros(channel_length)
+for i in range(0, len(energies0)):
+	energies0[i] = e
+#energies0 = np.array([e,e,e,e,e,e,e,e,e,e])
+
+id_ch.setEnergies(energies0)
+system_id = tensor(qubitL.twolevel(), id_ch.asQobj(), qubitR.twolevel())
+ptrace_id = system_id.ptrace(2)
+result_id = qubit.qubit(ptrace_id.full()[0][0], \
+	ptrace_id.full()[1][1], ptrace_id.full()[0][1])
+
+controlR = qubitEnt.ptrace(1)
+ctrlR = qubit.qubit(controlR.full()[0][0], \
+	controlR.full()[1][1], controlR.full()[0][1])
+
+#print result_rand.twolevel()
+#print result_id.twolevel()
+#print system_rand.ptrace(2)
+
 
 # arranging an axis along which to output the time evolution
-t = np.arange(0,10*np.pi,0.01)
-
-# calculating the fourier transform using the hardcoded functions from qubit.py
-expectsUpR = qubitR.spinUpFrac(t)
-expectsDownR = qubitR.spinDownFrac(t)
-
-fig, hardcoded = plt.subplots()
-hardcoded.plot( t, expectsUpR, label='Expect |0> - |G(t)_11|^2/2pi')
-hardcoded.plot( t, expectsDownR, label='Expect |1> - |G(t)_12|^2/2pi')
-plt.ylabel('Expectation')
-plt.xlabel('t')
-plt.title('Time evolution of the expectation value of a qubit')
-legend = hardcoded.legend(loc = 'upper center', bbox_to_anchor=(0.5,-0.1)\
-	,ncol=2, shadow = True)
-fig.savefig('Expectation values', bbox_extra_artists=(legend,), bbox_inches='tight')
-fig.subplots_adjust(bottom=0.2)
-frame = legend.get_frame()
-frame.set_facecolor('0.90')
-hardcoded.grid()
-# creating curried functions for G(E)[1][1] and G(E)[1][2]
-# up and down are now functions that can be evaluated as: up(E) and down(E)
-# for any E
-up = qubitR.fun_up()
-down = qubitR.fun_down()
-
-# calculating fourier transform using an iterative method. This may take up
-# to several minutes, since calculating the matrix inverse for every single 
-# value of E (as we integrate over it) is very computationally intensive, 
-# especially as the matrix becomes near-singular
-transUp = fourier.fourier_transform_loop(up, 15, 0.001, t)
-transDown = fourier.fourier_transform_loop(down, 15, 0.001, t)
-
-fig, fourier0 = plt.subplots()
-fourier0.plot(t, np.power(np.abs(transUp),2)/(4*np.pi**2))
-fourier0.plot(t, np.power(np.abs(transDown),2)/(4*np.pi**2))
-fourier0.set_title("-15 to 15, dx = 0.001")
-fourier0.grid()
+t = np.arange(0,50,0.01)
 """
+up = qubitR.GE11()
+down = qubitR.GE12()
+
 transUp = fourier.fourier_transform_loop(up, 5, 0.001, t)
 transDown = fourier.fourier_transform_loop(down, 5, 0.001, t)
 
+fig, temp = plt.subplots()
+temp.plot(t, np.power(np.abs(transUp),2)/(4*np.pi**2))
+temp.plot(t, np.power(np.abs(transDown),2)/(4*np.pi**2))
+"""
+# creating curried functions for G(E)[1][1] and G(E)[1][2]
+# up and down are now functions that can be evaluated as: G11(E) and G12(E)
+
+#GE11 = qubitR.GE11()
+#GE12 = qubitR.GE12()
+
+GE11_rand = result_rand.GE11()
+GE11_id = result_id.GE11()
+GE11R = qubitR.GE11()
+GE11_ctrl = ctrlR.GE11()
+
+transGE11_rand = fourier.fourier_transform_loop(GE11_rand, 5, 0.001, t)
+transGE11_id = fourier.fourier_transform_loop(GE11_id, 5, 0.001, t)
+transGE11R = fourier.fourier_transform_loop(GE11R, 5, 0.001, t)
+transGE11_ctrl = fourier.fourier_transform_loop(GE11_ctrl, 5, 0.001, t)
+
+fig, fourier1 = plt.subplots()
+#fourier3.plot(t, np.power(np.abs(transUp),2)/(4*np.pi**2))
+#fourier3.plot(t, np.power(np.abs(transDown),2)/(4*np.pi**2))
+rand11 = fourier1.plot(t, np.power(np.abs(transGE11_rand),2)/(4*np.pi**2), label = "Random energies channel")
+unif11 = fourier1.plot(t, np.power(np.abs(transGE11_id),2)/(4*np.pi**2), label = "Uniform energies channel")
+fourier1.set_title("G_11(t)")
+#fourier1.set_legend(handles =[rand11, unif11, orig11])
+fourier1.grid()
+
+
+GE12_rand = result_rand.GE12()
+GE12_id = result_id.GE12()
+GE12R = qubitR.GE12()
+GE12_ctrl = ctrlR.GE12()
+
+transGE12_rand = fourier.fourier_transform_loop(GE12_rand, 5, 0.001, t)
+transGE12_id = fourier.fourier_transform_loop(GE12_id, 5, 0.001, t)
+transGE12R = fourier.fourier_transform_loop(GE12R, 5, 0.001, t)
+transGE12_ctrl = fourier.fourier_transform_loop(GE12_ctrl, 5, 0.001, t)
+
 fig, fourier2 = plt.subplots()
-fourier2.plot(t, np.power(np.abs(transUp),2)/(4*np.pi**2))
-fourier2.plot(t, np.power(np.abs(transDown),2)/(4*np.pi**2))
-fourier2.set_title("-5 to 5, dx = 0.001")
+rand12 = fourier2.plot(t, np.power(np.abs(transGE12_rand),2)/(4*np.pi**2), label = "random energies channel")
+unif12 = fourier2.plot(t, np.power(np.abs(transGE12_id),2)/(4*np.pi**2), label = "uniform energies channel")
+fourier2.set_title("G_12(t)")
+#fourier2.legend(handles =[rand12, unif12, orig12])
 fourier2.grid()
 
-transUp = fourier.fourier_transform_loop(up, 50, 0.01, t)
-transDown = fourier.fourier_transform_loop(down, 50, 0.01, t)
-
 fig, fourier3 = plt.subplots()
-fourier3.plot(t, np.power(np.abs(transUp),2)/(4*np.pi**2))
-fourier3.plot(t, np.power(np.abs(transDown),2)/(4*np.pi**2))
-fourier3.set_title("-50 to 50, dx = 0.01")
+orig12 = fourier3.plot(t, np.power(np.abs(transGE12R),2)/(4*np.pi**2), label= "original qubit")
+ctrl12 = fourier3.plot(t, np.power(np.abs(transGE12_ctrl),2)/(4*np.pi**2), label= "ideally entangled")
 fourier3.grid()
-"""
-plt.show()
+
+fig, fourier4 = plt.subplots()
+orig11 = fourier4.plot(t, np.power(np.abs(transGE11R),2)/(4*np.pi**2), label= "Original qubit")
+ctrl11 = fourier4.plot(t, np.power(np.abs(transGE11_ctrl),2)/(4*np.pi**2), label= "ideally entangled")
+fourier4.grid()
+
 # this took 8 min to run...
+plt.show()
+
+"""
+E = np.arange(-5, 5, 0.001)
+GE_11 = map(GE11, E)
+GE_12 = map(GE12, E)
+
+
+fig, GE11 = plt.subplots()
+GE11.plot(E, np.real(GE_11))
+GE11.plot(E, np.imag(GE_11))
+GE11.set_title("G(E)_11")
+GE11.grid()
+
+
+fig, GE12 = plt.subplots()
+GE12.plot(E, np.real(GE_12))
+GE12.plot(E, np.imag(GE_12))
+GE12.set_title("G(E)_12")
+GE12.grid()
+"""
